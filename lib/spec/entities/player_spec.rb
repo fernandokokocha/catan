29b2a1490e50db57f7b66e7a8e3a74408b35b071
Bartosz Krajka <krajka.bartosz@gmail.com>
2018-09-09 18:41:15 +0200

    Refactor `player_spec.rb`

--- lib/spec/entities/player_spec.rb
+++ lib/spec/entities/player_spec.rb
@@ -1,25 +1,16 @@
 describe Player do
+  before do
+    @player = Player.new('Player', :blue)
+  end
+
   it 'has resources' do
-    expect{ Player.new('Player', :blue).resources }.not_to raise_error
+    expect{ @player.resources }.not_to raise_error
   end
 
-  it 'has ore resources' do
-    expect{ Player.new('Player', :blue).resources[:ore] }.not_to raise_error
-  end
-
-  it 'has lumber resource' do
-    expect{ Player.new('Player', :blue).resources[:lumber] }.not_to raise_error
-  end
-
-  it 'has wool resource' do
-    expect{ Player.new('Player', :blue).resources[:wool] }.not_to raise_error
-  end
-
-  it 'has brick resource' do
-    expect{ Player.new('Player', :blue).resources[:brick] }.not_to raise_error
-  end
-
-  it 'has grain resource' do
-    expect{ Player.new('Player', :blue).resources[:grain] }.not_to raise_error
+  RESOURCE_NAMES = [:ore, :lumber, :wool, :brick, :grain]
+  RESOURCE_NAMES.each do |resource_name|
+    it "has #{resource_name} resource" do
+      expect(@player.resources[resource_name]).not_to be_nil
+    end
   end
 end

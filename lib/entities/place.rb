61c243e1369e743c106873deeb2a9cc5a7d5886a
Bartosz Krajka <krajka.bartosz@gmail.com>
2019-01-22 21:35:18 +0100

    Refactor Place model

--- lib/entities/place.rb
+++ lib/entities/place.rb
@@ -5,23 +5,10 @@ class Place
     @index = index
     @roads = []
 
-    case index
-    when (1..6)
-      @layer = 1
-      base = (index - 1)
-      @side = 1 + (base / 1)
-      @spot = 1 + (base % 1)
-    when (7..24)
-      @layer = 2
-      base = (index - 7)
-      @side = 1 + (base / 3)
-      @spot = 1 + (base % 3)
-    else
-      @layer = 3
-      base = (index - 25)
-      @side = 1 + (base / 5)
-      @spot = 1 + (base % 5)
-    end
+    characteristics = calc_characteristics(index)
+    @layer = characteristics[:layer]
+    @side = 1 + (characteristics[:index_in_layer] / characteristics[:places_per_side])
+    @spot = 1 + (characteristics[:index_in_layer] % characteristics[:places_per_side])
   end
 
   def settle(player)
@@ -30,5 +17,18 @@ class Place
 
   def add_road(neighbour, player)
     @roads << [neighbour, player]
+  end
+
+  private
+
+  def calc_characteristics(index)
+    case index
+    when (1..6)
+      { layer: 1, index_in_layer: index - 1, places_per_side: 1 }
+    when (7..24)
+      { layer: 2, index_in_layer: index - 7, places_per_side: 3 }
+    else
+      { layer: 3, index_in_layer: index - 25, places_per_side: 5 }
+    end
   end
 end
